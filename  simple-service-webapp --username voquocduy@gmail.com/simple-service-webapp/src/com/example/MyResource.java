7b112a52921d435728976cff7ec50df5c41c5905
voquocduy@gmail.com <voquocduy@gmail.com@cced8839-7ad0-1058-7095-bbbd8054b30f>
2013-09-06 12:36:12 +0000

    First Import

---
+++  simple-service-webapp --username voquocduy@gmail.com/simple-service-webapp/src/com/example/MyResource.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("helloworld")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }  
+    
+    /*
+ // This method is called if HTML is request
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHtmlHello() {
+      return "<html> " + "<title>" + "Hello Jersey HTML2" + "</title>"
+          + "<body><h1>" + "Hello Jersey HTML2" + "</body></h1>" + "</html> ";
+    }
+    
+    // This method is called if XML is request
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayXMLHello() { 
+      return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey XML" + "</hello>";
+    }
+    
+  */
+
+    
+}
